9afd0560bd6d71fefdb3ad814e8bcd2dc365915f
glendasp <glendasp@gmail.com>
2016-11-07 22:56:35 -0600

    typos

--- urls.py
+++ urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls.defaults import *
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from . import views
 
 
 urlpatterns = [
     # Allow us to access the admin page
     url(r'^admin/', admin.site.urls),
     # Index page
-    # (r'^$', 'todolist.todoapp.views.index')
+    # (url('', include('index.urls')),
 ]
